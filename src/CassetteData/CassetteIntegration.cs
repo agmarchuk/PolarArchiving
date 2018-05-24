06912177c9d561e35b83195cd4367f9bc54e37b3
agmarchuk <mag@iis.nsk.su>
2018-05-24 14:57:10 +0700

    Новый вариант сервиса данных

---
+++ src/CassetteData/CassetteIntegration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using Polar.Cassettes.DocumentStorage;
+
+namespace CassetteData
+{
+    public class CassetteIntegration
+    {
+        private DStorage storage = null;
+        private DbAdapter _engine;
+        private DbAdapter engine { get { return _engine; } }
+        public CassetteIntegration(XElement config, bool networking)
+        {
+
+        }
+    }
+}
---
+++ src/CassetteData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CassetteData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start CassetteData module");
+        }
+    }
+}
