4bccb23836f20603ebbadeaf8706e1b118165ada
agmarchuk <mag@iis.nsk.su>
2019-01-31 20:21:14 +0700

    НАчал пробный проект по формированию сервиса данных

---
+++ src/CDService/Controllers/DataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CDService.Controllers
+{
+    //[Route("data")]
+    //[ApiController]
+    public class DataController : ControllerBase
+    {
+        // GET: data/get
+        [HttpGet]
+        public IActionResult Get(string id)
+        {
+            return new ObjectResult("ok.");
+        }
+
+    }
+}
