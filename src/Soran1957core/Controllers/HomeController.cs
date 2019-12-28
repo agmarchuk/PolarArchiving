465e85087391069692b449f39ff2b7e3efc5ce0c
agmarchuk <mag@iis.nsk.su>
2019-12-28 18:28:52 +0700

    Начал проект Soran1957core

---
+++ src/Soran1957core/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Soran1957core.Models;
+
+namespace Soran1957core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult P()
+        {
+            ContentResult cr = new ContentResult() { ContentType = "text/html" };
+            XElement html = new XElement("html",
+                new XElement("head", 
+                    new XElement("meta", new XAttribute("charset", "utf-8")),
+                    new XElement("link", new XAttribute("src", "/Styles.css")),
+                    null),
+                new XElement("body",
+                    new XElement("img", new XAttribute("src", "/logo1.jpg")),
+                    new XElement("div", 
+
+                        new XElement("h1", "Привет!"),
+                        null)));
+            cr.Content = "<!DOCTYPE html>\n" + html.ToString(); // (SaveOptions.DisableFormatting);
+            return cr;
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/Soran1957core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Soran1957core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
