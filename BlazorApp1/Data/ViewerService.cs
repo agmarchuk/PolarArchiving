1722568a8a0b68cfb616f6dacd1fbab93ebf74d3
agmarchuk <mag@iis.nsk.su>
2020-01-20 14:47:14 +0700

    Работаю над BlazorApp1

---
+++ BlazorApp1/Data/ViewerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BlazorApp1.Data
+{
+    public class ViewerService
+    {
+        public Task<XElement> SearchAsync(string searchstring)
+        {
+            var q = OAData.OADB.SearchByName(searchstring);
+            return Task.FromResult(new XElement("xresult", q));
+        }
+        public Task<XElement> GetItemAsync(string id)
+        {
+            var q = OAData.OADB.GetItemByIdBasic(id, false);
+            return Task.FromResult(q);
+        }
+    }
+}
