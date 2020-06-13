77c6764d0288ba273d0d35cc5269af0926856b07
Alexander Marchuk <mag@iis.nsk.su>
2020-06-13 11:44:14 +0300

    Группа изменений

--- Experiments/SoranCore2/StaticObjects.cs
+++ Experiments/SoranCore2/StaticObjects.cs
@@ -15,7 +15,7 @@ namespace SoranCore
             OAData.Ontology.Init(path + "ontology_iis-v12-doc_ruen.xml");
             OAData.OADB.Init(path);
             var xel = OAData.OADB.GetItemByIdBasic("newspaper_28156_1997_1", false);
-            Console.WriteLine(xel.ToString());
+            if (xel != null) Console.WriteLine(xel.ToString());
             //OAData.OADB.Load();
         }
 
@@ -32,6 +32,23 @@ namespace SoranCore
                 if (xlang?.Value == "ru") { break; }
             }
             return res;
+        }
+        private static string[] months = new[] { "янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек" };
+        public static string DatePrinted(string date)
+        {
+            if (date == null) return null;
+            string[] split = date.Split('-');
+            string str = split[0];
+            if (split.Length > 1)
+            {
+                int month;
+                if (Int32.TryParse(split[1], out month) && month > 0 && month <= 12)
+                {
+                    str += months[month - 1];
+                    if (split.Length > 2) str += split[2].Substring(0,2);
+                }
+            }
+            return str;
         }
     }
 
--- TestConsole/GatherPdfs.cs
+++ TestConsole/GatherPdfs.cs
@@ -21,6 +21,7 @@ namespace TestConsole
             Dictionary<string, string> dur = new Dictionary<string, string>();
 
 
+
             DirectoryInfo di = new DirectoryInfo(pdf_path);
             FileInfo[] fls = di.GetDirectories()
                 .SelectMany(d1 => d1.GetDirectories())
@@ -29,11 +30,15 @@ namespace TestConsole
                 ;
             Console.WriteLine($"nfiles={fls.Count()}");
 
+            Func<string, string> Fromfile = (fn) => fls.First(f => f.Name == fn).FullName;
+
             string[] dbins = 
             {
                 @"D:\FactographProjects\newspaper\meta\newspaper_current.fog",
-                @"D:\FactographProjects\newspaper adds\meta\newspaper adds_current.fog"
+                //@"D:\FactographProjects\newspaper adds\meta\newspaper adds_current.fog"
             };
+            string cassette_out = @"D:\FactographProjects\PA_newspapers\newspaper\";
+            string dbout = cassette_out + @"meta\newspaper_current.fog";
 
             string[] specialnames = 
             {"спецвыпуск", "приложение №1", "приложение№2", "спецвыпуск15ноября", "спецвыпуск22ноября",
@@ -56,75 +61,157 @@ namespace TestConsole
             "ДК_12)",
             };
 
+            XElement xout = null;
             foreach (string dbin in dbins)
             {
                 XElement xin = XElement.Load(dbin);
+                if (xout == null) xout = new XElement(xin.Name, xin.Attributes());
                 int idoc = 0;
-                foreach (XElement xel in xin.Elements("document"))
+                bool collectionNVSadded = false;
+                foreach (XElement xel in xin.Elements())
                 {
                     string id = xel.Attribute("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about").Value;
-                    XElement iisstore = xel.Element("iisstore");
-                    if (iisstore == null) continue;
-                    string documenttype = iisstore.Attribute("documenttype")?.Value;
-                    if (documenttype != "scanned/dz") continue;
-                    idoc++;
-                    string uri = iisstore.Attribute("uri")?.Value;
-                    string name = xel.Element("name").Value;
-
-                    // Попытка выявить специальное имя
-                    int pos = 0;
-                    for (; pos < specialnames.Length; pos++) {if (specialnames[pos]==name) break;}
-                    
-                    if (pos < specialnames.Length) name = specialfnames[pos]; 
-                    else if (name.Length == 1) name = "0" + name;
-                    else if (name.Length == 3 && name[0] == '0') name = name.Substring(1);
-                    else if (name.IndexOf('-') > 0)
+                    if (xel.Name == "document")
                     {
-                        //name = name.Substring(1);
-                        if (name.IndexOf('-') == 3) 
+                        // Не используем дополнительный фог-документ
+                        if (id == "newspaper_0014_0780") continue;
+                        XElement iisstore = xel.Element("iisstore");
+                        if (iisstore == null) continue;
+                        string documenttype = iisstore.Attribute("documenttype")?.Value;
+                        if (documenttype != "scanned/dz") continue;
+                        idoc++;
+                        string uri = iisstore.Attribute("uri")?.Value;
+                        XElement name_el = xel.Element("name");
+                        string name = name_el.Value;
+                        string suffix = name;
+
+                        // Попытка выявить специальное имя
+                        int pos = 0;
+                        for (; pos < specialnames.Length; pos++) {if (specialnames[pos]==name) break;}
+                        
+                        if (pos < specialnames.Length) 
                         {
-                            string[] parts = name.Split('-');
-                            string first = parts[0];
-                            if (first.Length == 3) first = first.Substring(1);
-                            string second = parts[1];
-                            if (second.Length == 1) second = "0" + second;
-                            name = first + "-" + second;
+                            suffix = specialfnames[pos]; 
                         }
+                        else
+                        {
+                            if (name.Length == 1) name = "0" + name;
+                            else if (name.Length == 3 && name[0] == '0') name = name.Substring(1);
+                            else if (name.IndexOf('-') > 0)
+                            {
+                                //name = name.Substring(1);
+                                if (name.IndexOf('-') == 3) 
+                                {
+                                    string[] parts = name.Split('-');
+                                    string first = parts[0];
+                                    if (first.Length == 3) first = first.Substring(1);
+                                    string second = parts[1];
+                                    if (second.Length == 1) second = "0" + second;
+                                    name = first + "-" + second;
+                                }
+                            }
+                            suffix = name;
+                        }
+
+                        string fromdate = xel.Element("from-date")?.Value;
+
+                        // строю имя файла
+                        string fname = fromdate + "_" + suffix + ".pdf";
+                        // ищу файл
+
+                        var query = fls.Where(f => f.Name == fname)
+                            .Count();
+                        if (query != 1)
+                        {
+                            Console.WriteLine($"No FILE: {fname} => {fromdate} {suffix}");
+                        }
+                        string ff = Fromfile(fname);
+                        Console.WriteLine($"FROM FILE: {ff}");
+
+                        // Заполняю словари
+                        did.Add(id, fname);
+
+                        if (!dfi.ContainsKey(fname))
+                        {
+                            dfi.Add(fname, id);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Err: double fname {fname} for id={id} stored {dfi[fname]}");
+                        }
+                        
+                        dur.Add(uri, id);
+                        // Меняю имя в элементе
+                        name_el.Value = name;
+                        // Переписываю в базу данных
+                        xout.Add(xel);
+
+                        // ================ Работа с pdf файлами =============
+                        // Теперь у меня в fname - имя файла, а в uri - место и новое имя файла
+                        string[] uri_parts = uri.Split('/');
+                        string folder_nom = uri_parts[uri_parts.Length - 2];
+                        string file_nom =  uri_parts[uri_parts.Length - 1];
+                        string nfname = cassette_out + "originals/" + folder_nom + "/" + file_nom  + ".pdf";
+                        // Проверка на наличие
+                        if (!File.Exists(nfname)) 
+                        {
+                            if (!Directory.Exists(cassette_out + "originals/" + folder_nom))
+                            {
+                                Directory.CreateDirectory(cassette_out + "originals/" + folder_nom);
+                            }
+                            string fff = Fromfile(fname);
+                            File.Copy(fff, nfname);
+                        }
+                        //Console.WriteLine($"file: {fname} uri: {uri} nfname: {nfname}");
+
+                        // ====================== OK ======================
                     }
-                    //else if (name.StartsWith("депутатский курьер_")) name = name.Substring("депутатский курьер_".Length);
-
-                    string fromdate = xel.Element("from-date")?.Value;
-                    //bool toprint = true;
-
-
-                    // строю имя файла
-                    string fname = fromdate + "_" + name + ".pdf";
-                    // ищу файл
-                    var query = fls.Where(f => f.Name == fname)
-                        .Count();
-                    if (query != 1)
+                    else if (xel.Name == "collection-member")
                     {
-                        Console.WriteLine($"No FILE: {fname} => {fromdate} {name}");
+                        // меняем <in-collection rdf:resource="newspaper_cassetteId" />
+                        // на <in-collection rdf:resource="newspaperNVScollection" />
+                        XElement incollection = xel.Element("in-collection");
+                        if (incollection != null 
+                            && incollection.Attribute("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource")?.Value == "newspaper_cassetteId")
+                        {
+                            if (!collectionNVSadded)
+                            {
+                                xout.Add(
+                                    new XElement("collection",
+                                        new XAttribute("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about", "newspaperNVScollection"),
+                                        new XElement("name", "Газета: За науку в Сибири/Наука в Сибири")),
+                                    new XElement("collection-member",
+                                        new XAttribute("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about", "nnewpaper_2249284"),
+                                        new XElement("in-collection", 
+                                            new XAttribute("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource", "newspaper_cassetteId")),
+                                        new XElement("collection-item", 
+                                            new XAttribute("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource", "newspaperNVScollection"))
+                                        )
+                                );
+                                collectionNVSadded = true;
+                            }
+                            XAttribute att = incollection.Attribute("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource");
+                            att.Value = "newspaperNVScollection";
+                        }
+                        xout.Add(xel);
                     }
-
-                    // Заполняю словари
-                    did.Add(id, fname);
-
-                    if (!dfi.ContainsKey(fname))
+                    else if (xel.Name == "scanned-page")
                     {
-                        dfi.Add(fname, id);
+                        // Не записываем
                     }
                     else
                     {
-                        Console.WriteLine($"Err: double fname {fname} for id={id} stored {dfi[fname]}");
+                        // Просто переписываем
+                        xout.Add(xel);
                     }
-                    
-                    dur.Add(uri, id);
                 }
                 Console.WriteLine($"ndocs={idoc}");
             }
             Console.WriteLine($"{did.Count} did elements");
             Console.WriteLine($"{dfi.Count} dfi elements");
+            Console.WriteLine($"{dur.Count} dur elements");
+
+            xout.Save(dbout);
         }
     }
 }
